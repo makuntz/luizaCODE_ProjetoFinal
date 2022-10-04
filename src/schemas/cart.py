33402b14a688e67abeb4cd340f397c749119af28
Lya <lyacarolina94@gmail.com>
2022-10-04 19:57:14 -0300

    Criando schemas base

---
+++ src/schemas/cart.py
@@ -0,0 +1,13 @@
+import datetime
+from decimal import Decimal
+from pydantic import BaseModel, Field
+
+from src.schemas.address import Address
+from src.schemas.user import UserSchema
+
+class OrderSchema(BaseModel):
+    user: UserSchema
+    price: Decimal = Field(max_digits=10, decimal_places=2)
+    paid: bool = Field(default=False)
+    create: datetime.datetime = Field(default=datetime.datetime.now())
+    address: Address
