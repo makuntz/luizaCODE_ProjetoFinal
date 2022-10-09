550fec5c007c703d55823ec2e8ede642050f9671
makuntz <maira.kuntz@animati.com.br>
2022-10-09 18:40:36 -0300

    ajustes

--- src/controllers/carts.py
+++ src/controllers/carts.py
@@ -1,19 +1,8 @@
 from pymongo import MongoClient
-# from bson.objectid import ObjectId
-from decimal import Decimal
-import datetime
 from fastapi import APIRouter, FastAPI
-from src.controllers.products import COLECAO_PRODUTO
-from src.models.persistencia_bd import obter_colecao
-from src.regras.regras import add_cart
+from src.regras.regras_cart import add_cart
 from src.schemas.cart import CartSchema
 
-# from src.models.cart import create_cart
-# from src.regras.regras import add_cart
-
-
-
-COLECAO_PRODUTO = obter_colecao("carts")
 
 rota_carts = APIRouter(
     prefix="/api/cart",
@@ -22,59 +11,10 @@ rota_carts = APIRouter(
 
 
 ##acrescentar id_prod uto na rota, para verificar se produto existe
-@rota_carts.post("/{user}")
-async def cart_crud(user):
-    return await add_cart(user)
-    # await connect_db()
-    
-    # cart_collection = db.cart_collection
-    
-    # ##essa variavel deve conter funcao de busca no banco
-    # cart = { 
-    #             "user": "123456",
-    #             "price": 111.22,
-    #             "paid": False,
-    #             "create": datetime.datetime.now()
-    #         }
+@rota_carts.post("/{id_user}/{id_product}")
+async def cart_crud():
+    return await add_cart()
    
-    # if cart["user"] != user:
-    #         await create_cart(
-    #             cart_collection,
-    #             dict(cart)
-    #         )
-    # else:
-    #     print('falhouuu')
-    
-    
-    
-    # await disconnect_db()
-    
-    # return cart
-
- 
-    
-    # users_collection = db.users_collection
-    # address_collection = db.address_collection   
-    
-  
-    
-    #email = "lu_domagalu@gmail.com"
-    # user = await get_user_by_email(
-    #     users_collection,
-    #     email
-    # )
-    
-    # address_is_true = await address_collection.find_one({"address.is_delivery": True})
-    
-    # cart =   {
-    # #     "user": '123456',
-    #     "price": 111.22,
-    #     "paid": False
-    # #     "create": datetime.datetime.now()
-    # #     #"address": 'address_is_true'
-                  
-    # } 
-      
    
     
         
