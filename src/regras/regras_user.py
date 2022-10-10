a9f84c32cfde9a517a4bb536fb60bb702cca6814
Lya <lyacarolina94@gmail.com>
2022-10-10 19:36:25 -0300

    testando o retorno get_user

--- src/regras/regras_user.py
+++ src/regras/regras_user.py
@@ -1,40 +1,62 @@
 
-import email
 from src.models.persistencia_bd import obter_colecao
 from bson.objectid import ObjectId
 import datetime
 from src.models.user import create_user, get_user_by_email
+import re
 
 
 COLECAO_USER = obter_colecao("users") 
+regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+
 
 user = { 
-                "name": "Beicola",
-                "email": "bruna@email.com",
+                "name": "lya",
+                "email": "amarelo@email.com",
                 "password": "senha123",
                 "is_active": True
             }
 
+# def check(email):
+#     if(re.search(regex,email)):  
+#         print("Email válido")  
+          
+#     else:  
+#         print("Email inválido")
+    
+
+
+
 
 async def add_user():
+    
+#     validate_email = check(user.email)   
+    user = { 
+                "name": "lya",
+                "email": "amarelo@email.com",
+                "password": "senha123",
+                "is_active": True
+            }
+    
+    
     
     await create_user(
         COLECAO_USER,
         dict(user)
-    )
-    
+        )
+
     print('DEU CERTOOOO')
     return user
 
 
 async def get_user():
-    
-    await get_user_by_email(
+    #email = "bruna@email.com"
+    response = await get_user_by_email(
         COLECAO_USER,
-        user["email"]
+        {'email': email}
     )
         
-    return user
+    return response
 
 
 # async def delete_user():
